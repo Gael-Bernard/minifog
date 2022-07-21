928101418ce6d3df13acf585d6f4c683405a8455
Gael-Bernard <gael.berbere@gmail.com>
2022-07-22 01:48:01 +0200

    Added first socket.io necessary files

--- src/front/main.ts
+++ src/front/main.ts
@@ -1,12 +1,23 @@
 import './style.css'
 import * as PIXI from "pixi.js"
+import { io, Socket } from "socket.io-client"
+import { ClientToServerEvents, ServerToClientEvents } from '../shared/networkTypes';
 
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
+  <div id="canvasarea">
     
   </div>
 `
 
 const app = new PIXI.Application({width:256, height:256});
-console.log(app)
+
+const canvasArea = document.getElementById("canvasarea");
+if(canvasArea == null)
+  throw "canvasarea div component missing from HTML";
+canvasArea.appendChild(app.view);
+
+
+
+const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io();
+socket.on("connect", () => console.log("Connected to server !") );
