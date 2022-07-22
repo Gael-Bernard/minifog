b6d48e9be24df6cd17c9cbb0ccf6b8ec5a4d6c8b
Gael-Bernard <gael.berbere@gmail.com>
2022-07-22 19:58:52 +0200

    Minor fixes, renaming and moving folders

--- src/app.ts
+++ src/app.ts
@@ -6,8 +6,14 @@ const app = express();
 
 const port = 3000;
 
+
+/*
 app.get('/', (_, res) => {
-  res.status(200).send("OK");
+  //res.status(200).send("src/front/index.html");
 });
+*/
+
+app.use(express.static("dist/front/public"));
+app.use(express.static("src/front/public"));
 
 app.listen(port, () => console.log(`Running on port ${port}`));
--- src/front/public/index.ts
+++ src/front/public/index.ts
@@ -1,23 +1,25 @@
-import './style.css'
 import * as PIXI from "pixi.js"
 import { io, Socket } from "socket.io-client"
-import { ClientToServerEvents, ServerToClientEvents } from '../shared/networkTypes';
+import { ClientToServerEvents, ServerToClientEvents } from '../../shared/networkTypes'
 
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div id="canvasarea">
     
   </div>
-`
+`;
 
-const app = new PIXI.Application({width:256, height:256});
+const pixi = new PIXI.Application({width:256, height:256});
 
 const canvasArea = document.getElementById("canvasarea");
 if(canvasArea == null)
   throw "canvasarea div component missing from HTML";
-canvasArea.appendChild(app.view);
+canvasArea.appendChild(pixi.view);
 
 
 
 const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io();
 socket.on("connect", () => console.log("Connected to server !") );
+
+
+export default pixi;
